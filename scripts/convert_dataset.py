75919d6ccbe1ee27f7181eac925836d39eedad98
Ethan Joseph <ethanj217@gmail.com>
2022-12-11 12:53:46 -0500

    add model output midis

--- scripts/convert_dataset.py
+++ scripts/convert_dataset.py
@@ -2,7 +2,10 @@ import os
 import numpy as np
 import pandas as pd
 import pretty_midi
+from miditok import OctupleMono, REMI
+from miditoolkit import MidiFile
 from tqdm import tqdm
+from pathlib import Path
 
 SEED = 42
 
@@ -31,17 +34,39 @@ def train_validate_test_split(df, train_percent=.7, validate_percent=.15, seed=S
 
 
 if __name__ == "__main__":
-    df = pd.DataFrame(columns=['name', 'text'])
+    df = pd.DataFrame(columns=['name', 'tokens'])
     data = []
-    for (dirpath, dirnames, filenames) in os.walk('../data/raw_midi/'):
-        for filename in tqdm(filenames):
-            midi_file = os.path.join(dirpath, filename)
-            try:
-                text = midi_to_text(midi_file)
-            except:
-                continue
-            data.append([filename[:-4], text])
-    df = pd.DataFrame(data, columns=['name', 'text'])
+    # Creates the tokenizer and list the file paths
+    tokenizer = REMI() 
+    paths = list(Path('../data/raw_midi').glob('**/*.mid'))
+
+    def midi_valid(midi) -> bool:
+        if any(ts.numerator != 4 for ts in midi.time_signature_changes):
+            return False  # time signature different from 4/*, 4 beats per bar
+        if midi.max_tick < 10 * midi.ticks_per_beat:
+            return False  # this MIDI is too short
+        return True
+
+    # Converts MIDI files to tokens saved as JSON files
+    for path in tqdm(paths):
+        try:
+            midi = MidiFile(path)
+            if midi_valid(midi):
+                data.append([path.name[:-4], tokenizer.midi_to_tokens(midi)[0]])
+        except:
+            continue
+
+
+    # for (dirpath, dirnames, filenames) in os.walk('../data/raw_midi/'):
+    #     for filename in tqdm(filenames):
+    #         midi_file = os.path.join(dirpath, filename)
+    #         try:
+    #             text = midi_to_text(midi_file)
+    #         except:
+    #             continue
+    #         data.append([filename[:-4], text])
+
+    df = pd.DataFrame(data, columns=['name', 'tokens'])
     df.to_csv('../data/full.csv', index=False)
     train, valid, test = train_validate_test_split(df)
     train.to_csv('../data/train.csv', index=False)
